dc7b5c5ae935476e0a82c85a78b2594fa77c5551
cybiere <cybiere@tutanota.com>
2020-12-12 18:59:24 +0100

    Added rule matching

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,102 @@
 use regex::Regex;
-use std::fmt;
 use std::str::FromStr;
+use std::io;
+use std::error::Error;
 
-#[derive(Debug, Clone)]
-struct LogParseError;
+#[derive(Debug)]
+struct MatchRule {
+    name: String,
+    process: String,
+    regex: Regex,
+}
 
-impl fmt::Display for LogParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "cannot parse log line")
+impl MatchRule {
+    fn is_match(&self, line: &LogLine) -> bool {
+        if self.process != line.process{
+            return false
+        }
+        self.regex.is_match(line.message.as_str())
     }
 }
 
 #[derive(Debug)]
 struct LogLine {
-    year: Option<u32>,
-    month: Option<u8>,
-    day: Option<u8>,
-    hour: Option<u8>,
-    minute: Option<u8>,
-    second: Option<u8>,
-    level: Option<String>,
-    process: Option<String>,
+    year: u32,
+    month: u8,
+    day: u8,
+    hour: u8,
+    minute: u8,
+    second: u8,
+    level: String,
+    process: String,
     pid: Option<u32>,
-    message: Option<String>,
+    message: String,
+    raw: String,
 }
 
 impl LogLine {
-    fn clean_value<T: FromStr>(caps: &regex::Captures,key :&str) -> Option<T> {
+    fn clean_value<T: FromStr>(caps: &regex::Captures,key :&str) -> Result<Option<T>,Box<dyn Error>> {
         match caps.name(key) {
             Some(value) => match value.as_str().parse::<T>(){
-                Ok(value) => Some(value),
-                Err(_) => None,
+                Ok(value) => Ok(Some(value)),
+                Err(_) => Err(format!("Could not parse '{:?}'",value).into()),
             },
-            None => None,
+            None => Ok(None),
         }
     }
 
-    fn from_string(line: &str, format :&Regex) -> Option<LogLine> {
+    fn from_string(line: &str, format :&Regex) -> Result<LogLine,Box<dyn Error>> {
         let caps = match format.captures(&line){
             Some(caps) => caps,
-            None => return None,
+            None => return Err("Line does not match specified format.".into()),
         };
-        let logline = LogLine{
-            year: LogLine::clean_value(&caps, "year"),
-            month: LogLine::clean_value(&caps, "month"),
-            day: LogLine::clean_value(&caps, "day"),
-            hour: LogLine::clean_value(&caps, "hour"),
-            minute: LogLine::clean_value(&caps, "minute"),
-            second: LogLine::clean_value(&caps, "second"),
-            level: LogLine::clean_value(&caps, "level"),
-            process: LogLine::clean_value(&caps, "process"),
-            pid: LogLine::clean_value(&caps, "pid"),
-            message: LogLine::clean_value(&caps, "message"),
-        };
-        Some(logline)
+        let year = LogLine::clean_value(&caps, "year")?.unwrap();
+        let month = LogLine::clean_value(&caps, "month")?.unwrap();
+        let day = LogLine::clean_value(&caps, "day")?.unwrap();
+        let hour = LogLine::clean_value(&caps, "hour")?.unwrap();
+        let minute = LogLine::clean_value(&caps, "minute")?.unwrap();
+        let second = LogLine::clean_value(&caps, "second")?.unwrap();
+        let level = LogLine::clean_value(&caps, "level")?.unwrap();
+        let process = LogLine::clean_value(&caps, "process")?.unwrap();
+        let pid = LogLine::clean_value(&caps, "pid")?;
+        let message = LogLine::clean_value(&caps, "message")?.unwrap();
+        
+        Ok(LogLine::build(year, month, day, hour, minute, second, level, process, pid, message, line))
     }
+
+    fn build(year :u32, month :u8, day :u8, hour :u8, minute :u8, second :u8, level :String, process :String, pid :Option<u32>, message :String, line : &str) -> LogLine{
+        LogLine {
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            second,
+            level,
+            process,
+            pid,
+            message,
+            raw: String::from(line),
+        }
+    }
+}
+
+fn read_line() -> Option<String> {
+    let mut line = String::new();
+    let readbytes = match io::stdin().read_line(&mut line){
+        Ok(n) => n,
+        Err(error) => { println!("Error : {}",error); return None }
+    };
+    if readbytes == 0 {
+        return None
+    }
+    if line.ends_with('\n') {
+        line.pop();
+        if line.ends_with('\r') {
+            line.pop();
+        }
+    }
+    return Some(line)
 }
 
 fn main() {
@@ -62,33 +104,26 @@ fn main() {
     (?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})\s
     (?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})\s
     (?P<level>[[:alpha:]]+)\s
-    (?P<process>[[:alpha:]]+)(\[(?P<pid>\d+)\])?:(\s
-    (?P<message>.+))?$").unwrap();
-    let line = "2020-11-23 01:37:13 info sshd[57587]: Disconnected from user borg 192.168.44.2 port 33598";
-    let line2 = "This is garbage";
-    let line3 = "2020-11-28 16:43:06 info kernel: device vethee57d00 left promiscuous mode";
-    let line4 = "2020-11-12 15:59:06 debug root:";
-    let line5 = "aze-11-12 15:59:06 debug root:";
+    (?P<process>[[:alpha:]]+)(\[(?P<pid>\d+)\])?:\s
+    (?P<message>.+)$").unwrap();
 
-    match LogLine::from_string(&line,&format){
-        Some(logline) => println!("Log line : {:?}",logline),
-        None => println!("Failed to parse line"),
-    };
-    match LogLine::from_string(&line2,&format){
-        Some(logline) => println!("Log line : {:?}",logline),
-        None => println!("Failed to parse line"),
-    };
-    match LogLine::from_string(&line3,&format){
-        Some(logline) => println!("Log line : {:?}",logline),
-        None => println!("Failed to parse line"),
-    };
-    match LogLine::from_string(&line4,&format){
-        Some(logline) => println!("Log line : {:?}",logline),
-        None => println!("Failed to parse line"),
-    };
-    match LogLine::from_string(&line5,&format){
-        Some(logline) => println!("Log line : {:?}",logline),
-        None => println!("Failed to parse line"),
+    let rule_sshd = MatchRule{
+        name: String::from("ssh-all"),
+        process: String::from("sshd"),
+        regex: Regex::new(r".").unwrap(),
     };
 
+    loop {
+        let line = match read_line(){
+            Some(line) => line,
+            None => break
+        }; 
+        let logline = match LogLine::from_string(&line,&format){
+            Ok(logline) => logline,
+            Err(_) => continue,
+        };
+        if rule_sshd.is_match(&logline){
+            println!(">> Match on rule {}. Raw line is: \n\t{}", rule_sshd.name, logline.raw);
+        }
+    }
 }
